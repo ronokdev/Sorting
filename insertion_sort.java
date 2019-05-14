0a3d0f2116e064119070bfa3e702de4355ee972b
Farhan Fuad Ronok <25183566+rounook@users.noreply.github.com>
2019-05-15 00:01:43 +0600

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,4 +1,4 @@
-package practice.sort.com;
+package sorting.algo.practive;
 
 import java.util.Arrays;
 
@@ -21,15 +21,20 @@ public class insertion_sort
         int firstSortedIndex =0;
         int currentElement;
 
-          for(int i = firstSortedIndex+1;i<arry.length;i++){     // unsorted part
-              currentElement = arry[i];
-              for(int j = firstSortedIndex;j>=0 && arry[i] > arry[j];j--) // sorted Part
-              {
-                    arry[j] = arry[j+1];
+        for(int unsorted_index = firstSortedIndex+1;unsorted_index<arry.length;unsorted_index++){
 
-              }
-              firstSortedIndex++;
-          }
+            currentElement = arry[unsorted_index];
+            int sorted_index;
+            boolean counter = false;
+
+            for( sorted_index = firstSortedIndex;sorted_index>0 && arry[unsorted_index] < arry[sorted_index];sorted_index--)
+            {
+                arry[sorted_index+1] = arry[sorted_index];
+                counter = true;
+
+            }
+            if(counter)arry[sorted_index] = currentElement;
+            firstSortedIndex++;
+        }
     }
-
 }
