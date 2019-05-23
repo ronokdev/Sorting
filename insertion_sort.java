2f300f65c627220cb16be887c71aac603dc8a3f2
Farhan Fuad Ronok <25183566+rounook@users.noreply.github.com>
2019-05-23 23:04:04 +0600

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,22 +1,25 @@
-package practice.sort.com;
+package sorting.algo.practive;
 
 import java.util.Arrays;
 
 public class insertion_sort
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         int[] array_to_sort = {5, 10, -10, 55, -100, 6, 66, 60, 1, 900, -1001};
+        System.out.println("Running Insertion Sort");
         arrayToPrint(array_to_sort);
         insertionSort(array_to_sort);
         arrayToPrint(array_to_sort);
     }
+
+
+
     public static void arrayToPrint(int[] peram)
     {
         System.out.println(Arrays.toString(peram));
     }
-    public static void insertionSort(int[] arry)
-    {
+
+    public static void insertionSort(int[] arry){
         // setting the first sorted index to 0
         int firstSortedIndex = 0;
         int currentElement;
@@ -26,7 +29,7 @@ public class insertion_sort
             int sorted_index;
             boolean counter = false;
             // will check the SORTED portion 🔴🟢🟡
-            for( sorted_index = firstSortedIndex;sorted_index>0 && arry[unsorted_index] < currentElement;sorted_index--)
+            for (sorted_index = firstSortedIndex; sorted_index >= 0 && currentElement < arry[sorted_index]; sorted_index--) {
                 arry[sorted_index + 1] = arry[sorted_index];
                 counter = true;
             }
